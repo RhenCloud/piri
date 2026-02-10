957b596efd3ca3422138d846d62e2362ad0d6285
RhenCloud <i@rhen.cloud>
2026-02-10 13:34:39 +0800

    :sparkles: Add nix build support

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Piri";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , rust-overlay
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.beta.latest.default;
+          rustc = pkgs.rust-bin.beta.latest.default;
+        };
+      in
+      {
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "piri";
+          version = "0.1.5";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+        };
+      }
+    );
+}
