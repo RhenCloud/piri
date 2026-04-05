bba9f81a6ea113d92630a0b8e8a2abfcccc0799e
RhenCloud <i@rhen.cloud>
2026-04-05 22:34:36 +0800

    :sparkles: Refactor flake to expose self.packages

--- flake.nix
+++ flake.nix
@@ -9,47 +9,52 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       rust-overlay,
       flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = pkgs.rust-bin.beta.latest.default;
-          rustc = pkgs.rust-bin.beta.latest.default;
-        };
-        piri = rustPlatform.buildRustPackage {
-          pname = "piri";
-          version = "0.1.5";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-        };
-      in
-      {
-        packages.default = piri;
-
-        nixosModules.default =
-          { pkgs, ... }:
-          {
-            imports = [ ./nix/module.nix ];
-            config.programs.piri.package = pkgs.lib.mkDefault piri;
+    let
+      outputs = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
           };
-        homeManagerModules.default =
-          { pkgs, ... }:
-          {
-            imports = [ ./nix/home-manager.nix ];
-            config.programs.piri.package = pkgs.lib.mkDefault piri;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = pkgs.rust-bin.beta.latest.default;
+            rustc = pkgs.rust-bin.beta.latest.default;
           };
-      }
-    );
+          piri = rustPlatform.buildRustPackage {
+            pname = "piri";
+            version = "0.1.5";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+          };
+        in
+        {
+          packages.default = piri;
+        }
+      );
+    in
+    outputs
+    // {
+      nixosModules.default =
+        { pkgs, ... }:
+        {
+          imports = [ ./nix/module.nix ];
+          config.programs.piri.package = nixpkgs.lib.mkDefault self.packages.${pkgs.system}.default;
+        };
+      homeManagerModules.default =
+        { pkgs, ... }:
+        {
+          imports = [ ./nix/home-manager.nix ];
+          config.programs.piri.package = nixpkgs.lib.mkDefault self.packages.${pkgs.system}.default;
+        };
+    };
 }
